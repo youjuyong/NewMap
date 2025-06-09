217a531249776313e53c21e04d8f81399609c3e9
youjuyong <dbwndyd518@naver.com>
2025-06-09 14:08:57 +0900

    환경설정 수정

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,3 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          "envName": "APP_ENV",
-          "moduleName": "@env",
-          "path": ".env",
-          "blocklist": null,
-          "allowlist": null,
-          "blacklist": null, 
-          "whitelist": null, 
-          "safe": false,
-          "allowUndefined": true,
-          "verbose": false
-        },
-      ],
-    ],
+module.exports = {
+  presets: ['babel-preset-expo'],
   };
-};
